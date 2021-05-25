079ada49e1b9644a73475901b4093824a62b0cf8
rifanid <adninsijawa.office@gmail.com>
2021-05-26 01:04:45 +0700

    update type Question

--- src/main/types/index.ts
+++ src/main/types/index.ts
@@ -1,7 +1,7 @@
 export type Category = {
   id?: number;
   name?: string;
-  descriptions?: string;
+  description?: string;
   created_at?: string;
   updated_at?: string;
 };
@@ -9,7 +9,7 @@ export type Category = {
 export type SubCategory = {
   id?: number;
   name?: string;
-  descriptions?: string;
+  description?: string;
   created_at?: string;
   updated_at?: string;
 };
@@ -25,14 +25,14 @@ export type Question = {
   [x: string]: any;
   id?: number;
   question?: string;
-  answer_a?: string;
-  answer_b?: string;
-  answer_c?: string;
-  answer_d?: string;
-  correct_answer_pg?: QuestionAnswer;
-  correct_answer_essay?: string;
+  answer_a?: string | null;
+  answer_b?: string | null;
+  answer_c?: string | null;
+  answer_d?: string | null;
+  correct_answer_pg?: string | QuestionAnswer | null;
+  correct_answer_essay?: string | null;
   category_id?: number;
   sub_category_id?: number;
   created_at?: string;
-  updated_at?: string;
+  updated_at?: string | null;
 };
