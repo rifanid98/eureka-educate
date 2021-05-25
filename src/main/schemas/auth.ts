92d4b6d9fe14e64120ec033b8ba717ebaf3329f7
rifanid <adninsijawa.office@gmail.com>
2021-05-25 16:49:04 +0700

    auth schema

---
+++ src/main/schemas/auth.ts
@@ -0,0 +1,30 @@
+import Joi from 'joi';
+
+/**
+ * Create your own joi schema here.
+ * Don't use Joi.object here! use plain object instead
+ *
+ * import this schemas to your controller
+ * or models to start validate the data.
+ *
+ * import FormValidation from 'path/to/utils/FormValidation.js';
+ * import {loginSchema} from 'path/to/this/file/js';
+ *
+ * const body = req.body;
+ * // or
+ * const body = {
+ *      username: 'root',
+ *      password: 'root'
+ * }
+ * const fieldToPatch = Object.keys(body);
+ * const valid = new FormValidation(loginSchema, body)                  // static validation
+ * const valid = new FormValidation(loginSchema, body, fieldToPatch)    // dynamic validation
+ *
+ */
+class Schemas {
+  static token = {
+    token: Joi.string().trim().required(),
+  };
+}
+
+export default Schemas;
