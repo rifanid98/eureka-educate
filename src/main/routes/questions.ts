b24982da0c364826288cfd73ab0a6172968b2ac4
rifanid <adninsijawa.office@gmail.com>
2021-05-26 12:05:31 +0700

    use multer upload file middleware

--- src/main/routes/questions.ts
+++ src/main/routes/questions.ts
@@ -2,6 +2,7 @@ import { Router } from 'express';
 
 /** Controllers */
 import QuestionsController from '../controllers/questions';
+import { upload } from '../utils/helpers/helpers';
 
 // /** Middleware */
 // import AuthMiddleware from '../middlewares/Auth';
@@ -24,8 +25,8 @@ class QuestionsRouter {
   public routes() {
     this.router.get('/', QuestionsController.get);
     this.router.get('/:id', QuestionsController.getById);
-    this.router.post('/', QuestionsController.post);
-    this.router.patch('/:id', QuestionsController.patch);
+    this.router.post('/', upload.single("image"), QuestionsController.post);
+    this.router.patch('/:id', upload.single("image"), QuestionsController.patch);
     this.router.delete('/:id', QuestionsController.delete);
   }
 }
